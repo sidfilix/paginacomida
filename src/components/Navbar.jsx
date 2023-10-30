6c2a6484ac5d79d8c14854e30417b03ad8ae2b1e
sid <elsidcrew@gmail.com>
2023-10-30 14:17:51 -0600

    se agrega el proyecto

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai'
+
+const Navbar = () => {
+    return (
+        <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+            <div className='flex items-center'>
+                <div className='cursor-pointer'>
+                    <AiOutlineMenu size={30} />
+                </div>
+                <h1 className='text- 2xl sm:text-3xl lg:text-4xl px-2'>
+                    Best<span className='font-bold'>Eats</span>
+                </h1>
+                <div className='hidden lg:flex items center bg-gray-200 rounded-full p-1 text-[14px]'>
+                    <p className='bg-black text-white rounded-full p-2'>Delivery</p>
+                    <p className="p-2">Pickup</p>
+                </div>
+            </div>
+            <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+                <AiOutlineSearch sixe={20} />
+                <input className="bg-trasparent p-2 w-full focus: outline-none" type="text" placeholder="search food" />
+            </div>
+        </div>
+    )
+
+}
+
+export default Navbar
